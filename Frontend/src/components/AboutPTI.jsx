f67252e800579ec102a3fbd2dde716e7774b9e26
pnjnlng <pan.jackson1@gmail.com>
2024-05-21 12:23:37 -0700

    Added button to display what PTI stands for, needs work though.

--- Frontend/src/components/AboutPTI.jsx
+++ Frontend/src/components/AboutPTI.jsx
@@ -16,9 +16,7 @@ const AboutPTI = () => {
                 <div>
                     <h2>What is PTI?</h2>
                     <p>
-                        PTI stands for Pakistan Tehreek-e-Insaf, which translates to Pakistan Movement for Justice.
-                        It is a political party in Pakistan founded by Imran Khan in 1996. The party's goal is to create
-                        a modern, democratic welfare state.
+                        PTI stands for player team impact.
                     </p>
                 </div>
             )}
